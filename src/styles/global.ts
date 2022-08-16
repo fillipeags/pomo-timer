0e942aa6f323b25e4cf336974a6bb28f83abd1fa
Fillipe Augusto <fillipe484@gmail.com>
2022-08-16 00:22:59 -0300

    chore: structures settings and libraries config

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['gray-500']};
+  }
+
+  body {
+    ${({ theme }) => css`
+      background: ${theme['gray-900']};
+      color: ${theme['gray-300']};
+    `};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
